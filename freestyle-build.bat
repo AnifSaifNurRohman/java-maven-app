3f1686ca75a048fa1015c1eb7f91f646eb7155a6
Anif <121415556+AnifSaifNurRohman@users.noreply.github.com>
2025-04-29 16:04:33 +0700

    Update freestyle-build.bat

--- freestyle-build.bat
+++ freestyle-build.bat
@@ -1,8 +1,1 @@
-echo Starting Node.js build...
-node app.js
-IF %ERRORLEVEL% NEQ 0 (
-  echo Node.js script failed!
-  exit /b %ERRORLEVEL%
-)
-echo Node.js build completed successfully.
-
+node --version
