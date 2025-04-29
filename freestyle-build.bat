85901c2eee8883743aedf961e2da5a501eb7dabf
Anif <121415556+AnifSaifNurRohman@users.noreply.github.com>
2025-04-29 16:03:27 +0700

    Update freestyle-build.bat

--- freestyle-build.bat
+++ freestyle-build.bat
@@ -1,3 +1,8 @@
-@echo off
-echo Starting Maven build...
-call mvn clean install
+echo Starting Node.js build...
+node app.js
+IF %ERRORLEVEL% NEQ 0 (
+  echo Node.js script failed!
+  exit /b %ERRORLEVEL%
+)
+echo Node.js build completed successfully.
+
